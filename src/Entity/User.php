6df937c385090eaa57e9adb7539a48dd391ff402
Langnito Constant <constant.langnito@etudiant.univ-rennes1.fr>
2020-04-21 23:04:43 +0200

    Ajout de la logique des roles et de la securite

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -84,6 +84,11 @@ class User implements UserInterface
     private $ads;
 
     /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Role", mappedBy="users")
+     */
+    private $userRoles;
+
+    /**
      * Permet d'initilslisé le slug
      * 
      * @ORM\PrePersist
@@ -101,6 +106,7 @@ class User implements UserInterface
     public function __construct()
     {
         $this->ads = new ArrayCollection();
+        $this->userRoles = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -259,5 +265,33 @@ class User implements UserInterface
     }
 
     public function eraseCredentials() {}
+
+    /**
+     * @return Collection|Role[]
+     */
+    public function getUserRoles(): Collection
+    {
+        return $this->userRoles;
+    }
+
+    public function addUserRole(Role $userRole): self
+    {
+        if (!$this->userRoles->contains($userRole)) {
+            $this->userRoles[] = $userRole;
+            $userRole->addUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserRole(Role $userRole): self
+    {
+        if ($this->userRoles->contains($userRole)) {
+            $this->userRoles->removeElement($userRole);
+            $userRole->removeUser($this);
+        }
+
+        return $this;
+    }
 
 }
